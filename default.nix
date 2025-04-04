153793c454f896901b9e6ec1b769f64540412bac
Stefan <git@stefn.co.uk>
2025-04-04 19:31:04 +0100

    Update all the Nix'y stuff

--- default.nix
+++ default.nix
@@ -1,57 +1,14 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-
-let
-  # Input source files
-  src = ./.;
-  nodeDeps = import ./node-deps.nix { inherit pkgs; };
-  inherit (nodeDeps) packageJSON nodeModules;
-
-in
-pkgs.stdenv.mkDerivation {
-  name = "kevinburkeservices-com";
-
-  src = builtins.filterSource (
-    path: type:
-    !(builtins.elem (baseNameOf path) [
-      "_site"
-      "node_modules"
-      ".git"
-    ])
-  ) src;
-
-  nativeBuildInputs = with pkgs; [
-    cacert
-    html-tidy
-    lightningcss
-    sass
-    yarn
-  ];
-
-  configurePhase = ''
-    export HOME=$TMPDIR
-    mkdir -p _site/style
-
-    cp -r ${nodeModules}/node_modules .
-    chmod -R +w node_modules
-    cp ${packageJSON} package.json
-  '';
-
-  buildPhase = ''
-    echo 'Building CSS'
-    sass --update src/_scss:_site/css --style compressed
-
-    echo 'Building site'
-    yarn --offline eleventy
-
-    echo 'Tidying HTML'
-    find _site -name "*.html" -exec tidy --wrap 80 --indent auto --indent-spaces 2 --quiet yes --tidy-mark no -modify {} \;
-  '';
-
-  installPhase = ''
-    mkdir -p $out
-    cp -r _site/* $out/
-    rm -rf node_modules _site package.json
-  '';
-}
+(import
+  (
+    let
+      flake-compat = fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/12c64ca55c1014cdc1b16ed5a804aa8576601ff2.tar.gz";
+        sha256 = "0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5";
+      };
+    in
+    "${flake-compat}/default.nix"
+  )
+  {
+    src = ./.;
+  }
+).defaultNix.packages.${builtins.currentSystem}.site
--- flake.nix
+++ flake.nix
@@ -1,85 +1,139 @@
 {
-  description = "KevinBurkeServices.com";
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs";
   };
+
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        nodeDeps = import ./node-deps.nix { inherit pkgs; };
-        inherit (nodeDeps) packageJSON nodeModules;
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        # "aarch64-linux"
+      ];
 
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
 
-        # Common build inputs
-        commonBuildInputs = with pkgs; [
-          html-tidy
-          sass
-          yarn
-          yarn2nix
-        ];
+      mkUtils =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          nodeDeps = import ./node-deps.nix { inherit pkgs; };
+          inherit (nodeDeps) packageJSON nodeModules;
 
-        # Helper function to create scripts
-        mkScript =
-          name:
-          (pkgs.writeScriptBin name (builtins.readFile ./bin/${name})).overrideAttrs (old: {
-            buildCommand = "${old.buildCommand}\n patchShebangs $out";
-          });
+          deps = with pkgs; [
+            html-tidy
+            sass
+            yarn
+          ];
 
-        # Helper function to create packages
-        mkPackage =
-          name:
-          pkgs.symlinkJoin {
-            inherit name;
-            paths = [ (mkScript name) ] ++ commonBuildInputs;
-            buildInputs = [ pkgs.makeWrapper ];
-            postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+          mkScript =
+            name:
+            let
+              base = pkgs.writeScriptBin name (builtins.readFile ./bin/${name});
+              patched = base.overrideAttrs (old: {
+                buildCommand = "${old.buildCommand}\n patchShebangs $out";
+              });
+            in
+            pkgs.symlinkJoin {
+              inherit name;
+              paths = [ patched ] ++ deps;
+              buildInputs = [ pkgs.makeWrapper ];
+              postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
+            };
+
+          scripts = [
+            "build"
+            "serve"
+            "dryrun"
+            "tidy_html"
+          ];
+
+          scriptPkgs = builtins.listToAttrs (
+            map (name: {
+              inherit name;
+              value = mkScript name;
+            }) scripts
+          );
+
+          site = pkgs.stdenv.mkDerivation {
+            name = "eleventy-site";
+            src = ./.;
+            buildInputs = deps ++ [ nodeModules ];
+
+            configurePhase = ''
+              ln -sf ${packageJSON} package.json
+              ln -sf ${nodeModules}/node_modules .
+            '';
+
+            buildPhase = ''
+              ${mkScript "build"}/bin/build
+              ${mkScript "tidy_html"}/bin/tidy_html
+            '';
+
+            installPhase = ''
+              cp -r _site $out
+            '';
+
+            dontFixup = true;
           };
+        in
+        {
+          inherit
+            pkgs
+            deps
+            mkScript
+            scripts
+            scriptPkgs
+            site
+            ;
+          inherit packageJSON nodeModules;
+        };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          u = mkUtils system;
+          inherit (u) scriptPkgs site;
+        in
+        scriptPkgs // { inherit site; }
+      );
 
-        # Script names
-        scripts = [
-          "build"
-          "serve"
-          "tidy_html"
-        ];
+      defaultPackage = forAllSystems (system: self.packages.${system}.site);
 
-        # Generate all packages
-        scriptPackages = builtins.listToAttrs (
-          map (name: {
-            inherit name;
-            value = mkPackage name;
-          }) scripts
-        );
-
-      in
-      rec {
-        defaultPackage = packages.serve;
-        packages = scriptPackages;
-
-        devShells = rec {
+      devShells = forAllSystems (
+        system:
+        let
+          u = mkUtils system;
+          inherit (u)
+            pkgs
+            deps
+            scriptPkgs
+            packageJSON
+            nodeModules
+            ;
+        in
+        rec {
           default = dev;
           dev = pkgs.mkShell {
-            buildInputs = commonBuildInputs ++ (builtins.attrValues packages);
+            buildInputs = deps ++ (builtins.attrValues scriptPkgs);
+
             shellHook = ''
-              rm -rf node_modules
-              rm -rf package.json
+              rm -rf node_modules package.json
               ln -sf ${packageJSON} package.json
               ln -sf ${nodeModules}/node_modules .
               echo "Development environment ready!"
-              echo "Run 'serve' to start development server"
-              echo "Run 'build' to build the site in the _site directory"
-              echo "Run 'tidy_html' to run html-tidy over each file in _site"
+              echo ""
+              echo "Available commands:"
+              echo " - 'serve'     - Start development server"
+              echo " - 'build'     - Build the site in the _site directory"
+              echo " - 'dryrun'    - Perform a dry run build"
+              echo " - 'tidy_html' - Format HTML files in _site"
+              echo ""
+              git pull
             '';
           };
-        };
-      }
-    );
+        }
+      );
+    };
 }
