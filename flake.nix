5279a0044ad250d2486fefcec179d82d3c3a3bf8
Stefan <git@stefn.co.uk>
2025-10-06 13:39:35 +0100

    Switch to Github/Bunny

--- flake.nix
+++ flake.nix
@@ -6,130 +6,54 @@
   outputs =
     { self, nixpkgs }:
     let
-      systems = [
-        "x86_64-linux"
-        # "aarch64-linux"
-      ];
+      systems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
 
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-
-      mkUtils =
+      makeEnvForSystem =
         system:
         let
-          pkgs = import nixpkgs { inherit system; };
-          nodeDeps = import ./node-deps.nix { inherit pkgs; };
-          inherit (nodeDeps) packageJSON nodeModules;
+          pkgs = import nixpkgs { system = system; };
 
-          deps = with pkgs; [
+          devDependencies = with pkgs; [
+            nodejs_23
             html-tidy
             sass
-            yarn
           ];
-
-          mkScript =
-            name:
-            let
-              base = pkgs.writeScriptBin name (builtins.readFile ./bin/${name});
-              patched = base.overrideAttrs (old: {
-                buildCommand = "${old.buildCommand}\n patchShebangs $out";
-              });
-            in
-            pkgs.symlinkJoin {
-              inherit name;
-              paths = [ patched ] ++ deps;
-              buildInputs = [ pkgs.makeWrapper ];
-              postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
-            };
-
-          scripts = [
-            "build"
-            "serve"
-            "dryrun"
-            "tidy_html"
-          ];
-
-          scriptPkgs = builtins.listToAttrs (
-            map (name: {
-              inherit name;
-              value = mkScript name;
-            }) scripts
-          );
-
-          site = pkgs.stdenv.mkDerivation {
-            name = "eleventy-site";
-            src = ./.;
-            buildInputs = deps ++ [ nodeModules ];
-
-            configurePhase = ''
-              ln -sf ${packageJSON} package.json
-              ln -sf ${nodeModules}/node_modules .
-            '';
-
-            buildPhase = ''
-              ${mkScript "build"}/bin/build
-              ${mkScript "tidy_html"}/bin/tidy_html
-            '';
-
-            installPhase = ''
-              cp -r _site $out
-            '';
-
-            dontFixup = true;
-          };
         in
         {
-          inherit
-            pkgs
-            deps
-            mkScript
-            scripts
-            scriptPkgs
-            site
-            ;
-          inherit packageJSON nodeModules;
+          inherit pkgs;
+          devEnv = {
+            inherit pkgs;
+            dependencies = devDependencies;
+          };
         };
     in
     {
-      packages = forAllSystems (
-        system:
-        let
-          u = mkUtils system;
-          inherit (u) scriptPkgs site;
-        in
-        scriptPkgs // { inherit site; }
-      );
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.site);
-
       devShells = forAllSystems (
         system:
         let
-          u = mkUtils system;
-          inherit (u)
+          env = makeEnvForSystem system;
+          inherit (env.devEnv)
             pkgs
-            deps
-            scriptPkgs
-            packageJSON
-            nodeModules
+            dependencies
             ;
         in
-        rec {
-          default = dev;
-          dev = pkgs.mkShell {
-            buildInputs = deps ++ (builtins.attrValues scriptPkgs);
+        {
+          default = pkgs.mkShell {
+            buildInputs = dependencies;
 
             shellHook = ''
-              rm -rf node_modules package.json
-              ln -sf ${packageJSON} package.json
-              ln -sf ${nodeModules}/node_modules .
-              echo "Development environment ready!"
-              echo ""
-              echo "Available commands:"
-              echo " - 'serve'     - Start development server"
-              echo " - 'build'     - Build the site in the _site directory"
-              echo " - 'dryrun'    - Perform a dry run build"
-              echo " - 'tidy_html' - Format HTML files in _site"
-              echo ""
+              cat <<EOF
+
+              Development environment ready!
+
+              Available commands:
+               - 'npm run serve'   # Start development server
+               - 'npm run build'   # Build the site in the _site directory
+               - 'npm run tidy'    # Format HTML files in _site
+
+              EOF
+
               git pull
             '';
           };
